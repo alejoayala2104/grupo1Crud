e134cdaf728e725dedd80fed9934c13932ed4017
Alejandro Ayala <alejoayala2104@gmail.com>
2023-01-12 17:08:26 -0500

    eliminar

---
+++ del_user2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("head.php") ?>
+</head>
+
+<body>
+    <?php include("menu.php"); ?>
+    <div class="container">
+       <h3>Resultado de la eliminación</h3>
+        <?php
+            include("conecta.php");
+            $bd = conectar();
+
+            $ced = $_POST["victima"];
+
+            $sql = "delete from usuarios where id = '$ced'";
+            // echo $sql;
+
+            $res = mysqli_query($bd,$sql);
+
+            if (!$res){
+                echo "<div class='alert alert-danger' role='alert'><b>Error</b><br>Registro no eliminado!!<br>" . mysqli_errno($bd) ." - " . mysqli_error($bd) . "</div>";
+            }
+            else {
+                echo "<div class='alert alert-success' role='alert'><b>Información</b><br>Registro Eliminado con éxito!!</div>";
+            }
+            mysqli_close($bd);
+        ?>
+
+
+    </div>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
